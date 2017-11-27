b9d160ed881cadfb79733d668e277e675d7a2ead
Dave Willkomm <david.willkomm@fidelitylife.com>
2017-11-27 16:14:48 -0600

    Fixed install script to again work with Homebrew
    Duh, I made the env vars optional so that at least Homebrew could specify different values

--- bin/install_ngx_mruby.sh
+++ bin/install_ngx_mruby.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -evx
 
+if [[ $(which brew) ]]; then
+  brew_prefix_openssl="$(brew --prefix openssl)"
+fi
+
 # Clean
 cd vendor
 rm -fr ngx_mruby-*
@@ -16,21 +20,30 @@ cd ngx_mruby-*
 git init
 git add mruby/build_config.rb
 
-# Build
-nginx_config_opt_env="--prefix=$(pwd)/build/nginx --with-http_ssl_module"
-if [ $(which brew) ]; then
-  brew update
-  brew install openssl
-  openssl_path=$(brew --prefix openssl)
-  cflags="-I${openssl_path}/include"
+# Set config options (these are optional so that the Homebrew formula can specify different values)
+if [[ -z ${NGINX_CONFIG_OPT_ENV} ]]; then
+  NGINX_CONFIG_OPT_ENV="--prefix=$(pwd)/build/nginx --with-http_ssl_module"
 
-  NGINX_CONFIG_OPT_ENV="${nginx_config_opt_env} --with-cc-opt=${cflags}" \
-    NGX_MRUBY_CFLAGS="${cflags}" \
-    NGX_MRUBY_LDFLAGS="-L${openssl_path}/lib -lcrypto" \
-    ./build.sh
-else
-  NGINX_CONFIG_OPT_ENV="${nginx_config_opt_env}" ./build.sh
+  if [[ -n ${brew_prefix_openssl} ]]; then
+    NGINX_CONFIG_OPT_ENV+=" --with-cc-opt=-I${brew_prefix_openssl}/include"
+  fi
 fi
+if [[ -z ${NGX_MRUBY_CFLAGS} && -n ${brew_prefix_openssl} ]]; then
+  NGX_MRUBY_CFLAGS="-I${brew_prefix_openssl}/include"
+fi
+if [[ -z ${NGX_MRUBY_LDFLAGS} ]]; then
+  NGX_MRUBY_LDFLAGS='-lcrypto'
+
+  if [[ -n ${brew_prefix_openssl} ]]; then
+    NGX_MRUBY_LDFLAGS+=" -L${brew_prefix_openssl}/lib"
+  fi
+fi
+
+# Build
+NGINX_CONFIG_OPT_ENV="${NGINX_CONFIG_OPT_ENV}" \
+  NGX_MRUBY_CFLAGS="${NGX_MRUBY_CFLAGS}" \
+  NGX_MRUBY_LDFLAGS="${NGX_MRUBY_LDFLAGS}" \
+  ./build.sh
 
 # Install
 LDFLAGS="-lcrypto" make install
